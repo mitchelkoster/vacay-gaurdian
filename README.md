01e9f288cb31f1a5e81343a6d47e640af3414949
Mitchel Koster <2762686+mitchelkoster@users.noreply.github.com>
2026-07-03 01:00:46 +0200

    Added a test notification

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Vacay Gaurdian
+
 Automatically take snapshots of my vacation hours from [https://blpl.greythr.com/](https://blpl.greythr.com/) so I can keep a ledger of vacation hours available.
 
 ## Usage
+
 When ran in normal mode you will be prompted for credentials before gathering results under `~/Documents/vacay` or `C:\Users\<username>\Documents\vacay`.
 
 ```bash
@@ -9,6 +11,12 @@ go run ./main.go
 
 GreytHR username:
 GreytHR password:
+```
+
+If you ever need to remove your password:
+
+```bash
+secret-tool clear attribute-name attribute-value
 ```
 
 To enable debugging add the following `environment` variables:
@@ -24,37 +32,39 @@ export GREYTHR_DEBUG="true"
 Logs are stored in `logs/` folder with both **screenshots** and a **ledger**.
 
 ### Screenshots
+
 Every time Vacay Gaurdian runs it stores proof-of-leave in the `logs/` folder with a file name corresponding to `dd-MM-YYYY` *(European date notation)*.
 
 The screenshot below is an example capture for `logs/30-03-2025.png`.
 
 ![Example Hours](example.png)
 
-
 ### Ledger
+
 Every time Vacay Gaurdian runs it updates a leder with leave balances over time in `logs/vacation.log`.
 
 The screenshot below is an example capture of just one entry in the log.
 
 ```
 [30-03-2025]
- - Leave Name: 		 Loss Of Pay 
- - Balance Count: 	0.00
- - Granted Count: 	0.00
+ - Leave Name:    Loss Of Pay 
+ - Balance Count:  0.00
+ - Granted Count:  0.00
 
- - Leave Name: 		 Comp - Off 
- - Balance Count: 	0.00
- - Granted Count: 	0.00
+ - Leave Name:    Comp - Off 
+ - Balance Count:  0.00
+ - Granted Count:  0.00
 
- - Leave Name: 		 Paternity Leave 
- - Balance Count: 	5.00
- - Granted Count: 	5.00
+ - Leave Name:    Paternity Leave 
+ - Balance Count:  5.00
+ - Granted Count:  5.00
 
- - Leave Name: 		 Casual Leave - Statutory 
- - Balance Count: 	22.28
- - Granted Count: 	23.28
+ - Leave Name:    Casual Leave - Statutory 
+ - Balance Count:  22.28
+ - Granted Count:  23.28
 
- - Leave Name: 		 Casual Leave - Non-Statutory 
- - Balance Count: 	5.00
- - Granted Count: 	5.00
+ - Leave Name:    Casual Leave - Non-Statutory 
+ - Balance Count:  5.00
+ - Granted Count:  5.00
  ```
+
--- main.go
+++ main.go
@@ -1,95 +1,28 @@
 package main
 
 import (
-	"bufio"
+	_ "embed"
 	"fmt"
 	"log"
 	"os"
-	"strings"
-	"syscall"
 	"time"
 
 	"example/vacay/greythr"
 	"example/vacay/storage"
 
-	"github.com/zalando/go-keyring"
-	"golang.org/x/term"
+	"github.com/gen2brain/beeep"
 )
 
-func readUserName(display string, reader *bufio.Reader) (string, error) {
-	fmt.Print(display)
-
-	username, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatalf("could not read user input: %v", err)
-		return "", err
-	}
-
-	username = strings.TrimSuffix(username, "\r\n")
-	username = strings.TrimSpace(username)
-
-	return username, nil
-}
-
-func readPassword(display string, reader *bufio.Reader) (string, error) {
-	fmt.Print(display)
-	fmt.Println()
-
-	password, err := term.ReadPassword(int(syscall.Stdin))
-	if err != nil {
-		log.Fatalf("could not read user input: %v", err)
-		return "", err
-	}
-
-	return string(password), nil
-}
-
-func getCredentials() (string, string, error) {
-	var err error
-	service := "vacay-gaurdian"
-
-	// Grab from enviroment if possible
-	username := os.Getenv("GREYTHR_USERNAME")
-	password := os.Getenv("GREYTHR_PASSWORD")
-
-	if username != "" && password != "" {
-		return username, password, nil
-	}
-
-	// Obtain username from STDIN
-	reader := bufio.NewReader(os.Stdin)
-	username, err = readUserName("GreytHR username: ", reader)
-	if err != nil {
-		log.Fatal("Could not read username")
-	}
-
-	// Check local OS keyring for password
-	password, err = keyring.Get(service, username)
-	if err != nil {
-		fmt.Println("Not in keyring")
-		// If not found, prompt the user
-		password, err = readPassword("GreytHR password: ", reader)
-		if err != nil {
-			log.Fatal("Could not read password from STDIN")
-		}
-
-		// Store user provided password
-		err = keyring.Set(service, username, password)
-		fmt.Println("Setting:", service, username, password)
-		if err != nil {
-			log.Fatal("Could not set password in keyring")
-		}
-	}
-
-	return username, password, err
-}
+//go:embed resources/info.png
+var icon []byte
+var appName = "Vacay Gaurdian"
 
 func main() {
 	currentTime := time.Now().Local()
 	currentDate := currentTime.Format("02-01-2006")
 
 	// Grab credentials and store in keychain
-	username, password, err := getCredentials()
+	username, password, err := GetCredentials()
 	if err != nil {
 		log.Fatalf("Error reading credentails: %v", err)
 	}
@@ -141,5 +74,12 @@ func main() {
 	err = greythr.ClosePlaywright(pw, browser)
 	if err != nil {
 		log.Fatalf("Could not close Playwright: %v", err)
+	}
+
+	// Send Notification
+	beeep.AppName = appName
+	err = beeep.Notify("Complete", "This is a test!", icon)
+	if err != nil {
+		panic(err)
 	}
 }
